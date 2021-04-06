bc801bc7f79d9fc68e33a9ba008df505daee4702
Minkyoung Kim <mineh.kim@gmail.com>
2021-04-05 23:00:21 -0400

    Update main.js

--- session-01/demo-showhide/common/main.js
+++ session-01/demo-showhide/common/main.js
@@ -1,19 +1,15 @@
+$(document).ready(function() {
+  $('button').click(function() {
+    // 1. toggle items — comment out each lines and see what happens on the page!
+    $('div').toggle();
+    // $('.group1').toggleClass('red');
+    // $('.group2').toggleClass('blue');
 
-// 1. toggle items — comment out each lines and see what happens on the page!
-  $(document).ready(function() {
-    $('button').click(function() {
-      $('div').toggle();
-      // $('.group1').toggleClass('red');
-      // $('.group2').toggleClass('blue');
-    });
-  });
-
-// 2. toggle the copy of the button
- $('button').on('click', function() {
+    // 2. toggle the copy of the button
     if ($(this).text() == $(this).data('copy-swap')) {
       $(this).text($(this).data('copy-original')) 
     } else {
       $(this).text($(this).data('copy-swap'));
     }
   });
-  
+});
